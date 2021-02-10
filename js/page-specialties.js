64561e9857aaf8ac1a3764ad9d6a42bd8b0f951a
Dusttt <lucchanphong1997@gmail.com>
2021-02-10 20:32:49 +0700

    finish menu page style (need adjust the js) , add img of menu dishes , adjust style in page specialties css

--- js/page-specialties.js
+++ js/page-specialties.js
@@ -1,9 +1,17 @@
 document.addEventListener('DOMContentLoaded',function(){
     const links = document.querySelectorAll('.page-specialties__select .select');
-    
-    links.forEach(link => {
+    const menu = document.querySelectorAll('.page-specialties__main-wrapper');
+
+    links.forEach((link,index) => {
         link.addEventListener('click',function (e) {
             e.preventDefault();
+            for (let i = 0; i < links.length; i++) {
+                links[i].classList.remove('active');
+                menu[i].classList.remove('active');
+            }
+            this.classList.add('active');
+            menu[index].classList.add('active');
+
         })
     });
 
